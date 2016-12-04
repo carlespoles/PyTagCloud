1a940337cadb3928b7f40a7e905908f43dc871fb
Carles Poles-Mielgo <carles.poles@gmail.com>
2016-12-04 13:49:44 -0800

    updated to python 3

--- pytagcloud/lang/counter.py
+++ pytagcloud/lang/counter.py
@@ -2,23 +2,27 @@
 import re
 from pytagcloud.lang.stopwords import StopWords
 from operator import itemgetter
-from collections import defaultdict
 
 def get_tag_counts(text):
     """
     Search tags in a given text. The language detection is based on stop lists.
     This implementation is inspired by https://github.com/jdf/cue.language. Thanks Jonathan Feinberg.
     """
-    words = map(lambda x:x.lower(), re.findall(r"[\w']+", text, re.UNICODE))
-    
-    s = StopWords()     
+    words = map(lambda x:x.lower(), re.findall(r'\w+', text, re.UNICODE))
+    words = list(words) #** Added this
+
+    s = StopWords()
     s.load_language(s.guess(words))
-    
-    counted = defaultdict(int)
-    
+
+    counted = {}
+
     for word in words:
+        print(word)
         if not s.is_stop_word(word) and len(word) > 1:
-            counted[word] += 1
-      
-    return sorted(counted.iteritems(), key=itemgetter(1), reverse=True)
+            if word in counted: #** if counted.has_key(word):
+                counted[word] += 1
+            else:
+                counted[word] = 1
+    #** iteritems to items
+    return sorted(counted.items(), key=itemgetter(1), reverse=True)
     
